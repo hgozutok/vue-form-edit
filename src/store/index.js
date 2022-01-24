6bfb3546a6e40acf89cedac257e7de4b4fbfd68f
Hugo <huseyin@gozutok.info>
2022-01-24 16:28:44 +0300

    4. commit

--- src/store/index.js
+++ src/store/index.js
@@ -1,20 +1,14 @@
 import { createStore } from "vuex";
 import { userModule } from "./userStore";
 import { counterModule } from "./counter";
+import { modalModule } from "./modalStore";
 export default createStore({
-  state: {
-   
-
-  },
-  mutations: {
-
-  },
-  actions: {
-  
-  },
+  state: {},
+  mutations: {},
+  actions: {},
   modules: {
-userM: userModule,
-counterM: counterModule,
-  }
-
+    userM: userModule,
+    counterM: counterModule,
+    modalM: modalModule,
+  },
 });
--- src/views/ParentView.vue
+++ src/views/ParentView.vue
@@ -1,15 +1,19 @@
 <template>
   <Parent />
   {{ JSON.stringify(this.$store.state.userM.user.token) }}
+  {{ JSON.stringify(this.$store.state.modalM.isShow) }}
   <input
     type="button"
     @click="clickMetoChangeState"
     value="clickMetoChangeState"
   />
+  <input type="button" @click="modalShow" value="Modal" />
+  <ModalDialog />
 </template>
 
 <script>
 import Parent from "@/components/child/Parent.vue";
+import ModalDialog from "@/components/dialog/ModalDialog.vue";
 
 export default {
   name: "ParenView",
@@ -23,9 +27,20 @@ export default {
       //this.$store.commit("setUserName", "Hugo");
       this.$store.dispatch("userLogin");
     },
+    modalShow() {
+      this.$store.dispatch("setTitle", "This will be deleted");
+      this.$store.dispatch("setContent", "Are you sure?");
+      console.log(this.$store.state.modalM.isShow);
+      this.$store.dispatch("setIsShow", !this.$store.state.modalM.isShow);
+    },
+  },
+  created() {
+    // this.$store.dispatch("userLogin");
+    console.log(this.$store.state.modalM.isShow);
   },
   components: {
     Parent,
+    ModalDialog,
   },
   computed: {
     doneTodosCount() {
